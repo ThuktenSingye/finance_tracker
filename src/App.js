b90f3e5017a3a6e421999e4d45a3c0296ea7a696
Thukten Singye <thuktensingye2163@gmail.com>
2023-03-02 06:40:15 +0600

    set up auth

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
 import Home from './pages/home/Home'
 import Navbar from './components/Navbar';
+
+import useAuthContext from './hooks/useAuthContext';
+
 function App() {
+  const {authIsReady} = useAuthContext()
+
   return (
     <div className="app">
-      <BrowserRouter>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/signup">
-            <SignUp/>
-          </Route>
-
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar/>
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/signup">
+              <SignUp/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+        )}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,30 @@ import React from 'react'
 import styles from './Navbar.module.css'
 import useLogOut from '../hooks/useLogOut'
 import { Link } from 'react-router-dom'
+import useAuthContext from '../hooks/useAuthContext'
 function Navbar() {
   const {logout} = useLogOut()
+  const {user} = useAuthContext()
   return (
     <div className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
+        {!user && (<>
         <li><Link to='./login'>Login</Link></li>
         <li><Link to='./signup'>SignUp</Link></li>
-        <li>
+        </>
+        )}
+        {user && (
+          <>
+          <li>hello, {user.displayName}</li>
           <button className='btn' onClick={logout}>Logout</button>
-        </li>
+          </>
+        )}
       </ul>
     </div>
   )
 }
-
 export default Navbar
+// after loging in, when we refresh, then we no longer see the login status thouh in firebase it is log in
+// after refresh, we are updating user context based on login,signup but we are not doing ithence it is null
+// for that we can check in firebase if user is login in.if so then dont render component 
